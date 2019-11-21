c9920e2b4c9e00abf93741416b38b8e7887c9e95
shiminghua <shiminghua2008@qq.com>
2019-11-21 16:16:45 +0800

    study dart

---
+++ study/a_base_dart_program.dart
@@ -0,0 +1,12 @@
+// 定义一个方法
+printNumber(num aNumber) {
+  print('The number is $aNumber');
+}
+
+// 主函数，这是程序执行的入口
+main() {
+  // 定义并初始化一个变量
+  var number = 86;
+  // 调用一个方法
+  printNumber(number);
+}
